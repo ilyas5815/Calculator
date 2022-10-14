db775871a92ee23bdb2df9d8c4e98fcb932dc77c
PITSOLUTION\ilyas.md <ilyas.md@pitsolutions.com>
2022-10-14 14:24:00 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+function clearr(){
+    document.getElementById('dis_play').value = " ";
+}
+function one(){
+    document.getElementById('dis_play').value += "1";
+}
+function two(){
+    document.getElementById('dis_play').value += "2";
+}
+function three(){
+    document.getElementById('dis_play').value += "3";
+}
+function plus(){
+    document.getElementById('dis_play').value += "+";
+}
+function four(){
+    document.getElementById('dis_play').value += "4";
+}
+function five(){
+    document.getElementById('dis_play').value += "5";
+}
+function six(){
+    document.getElementById('dis_play').value += "6";
+}
+function minus(){
+    document.getElementById('dis_play').value += "-";
+}
+function seven(){
+    document.getElementById('dis_play').value += "7";
+}
+function eight(){
+    document.getElementById('dis_play').value += "8";
+}
+function nine(){
+    document.getElementById('dis_play').value += "9";
+}
+function div(){
+    document.getElementById('dis_play').value += "/";
+}
+function equal(){
+    document.getElementById('dis_play').value = 
+    eval(document.getElementById('dis_play').value);
+}
+function mult(){
+    document.getElementById('dis_play').value += "*";
+}
+function zero(){
+    document.getElementById('dis_play').value += "0";
+}
